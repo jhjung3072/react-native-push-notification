be289aa2265079a3c1bce2a28ba53c10b60a9ea4
정재호 <61670291+jhjung3072@users.noreply.github.com>
2024-01-24 17:49:31 +0900

    Update RNPushNotificationHelper.java
    //notification.setCategory(NotificationCompat.CATEGORY_CALL);

--- android/src/main/java/com/dieam/reactnativepushnotification/modules/RNPushNotificationHelper.java
+++ android/src/main/java/com/dieam/reactnativepushnotification/modules/RNPushNotificationHelper.java
@@ -442,7 +442,7 @@ public class RNPushNotificationHelper {
             }
 
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                notification.setCategory(NotificationCompat.CATEGORY_CALL);
+                //notification.setCategory(NotificationCompat.CATEGORY_CALL);
 
                 String color = bundle.getString("color");
                 int defaultColor = this.config.getNotificationColor();
